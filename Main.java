23e29f75e89483c07c1da7e5053b0d24f96b511a
whatsmyname0 <81788202+whatsmyname0@users.noreply.github.com>
2024-12-04 18:21:24 +0000

    yes

--- Main.java
+++ Main.java
@@ -4,31 +4,82 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+
   }
 
-  public static void printNTimes(String word, int N)
+  public int getPlayer2Move(int round)
   {
-    // code solution here
+  int result;
+  if ((round < maxRounds) || (coin  > 3))
+  {
+    if (round % 3 == 0)
+    {
+      result = 3;
+    }
+    else if (round % 2 == 0)
+    {
+      result = 2;
+    }
+    else
+    {
+      result = 1;
+    }
+  }
+  return result;
   }
 
-  public static void printNums()
+  public void playGame()
   {
-    // code solution here
+    int round;
+    int coin1 = startingCoins;
+    int coin2 = startingCoins;
+    for (int round = 0; round < maxRounds; i++)
+    {
+      getPlayer1Move();
+      getPlayer2Move(round);
+      if ((coin1 < 3) || (coin2 < 3))
+      {
+        if (coin1 > coin2)
+        {
+          return ("Player 1 wins");
+        }
+        if (coin1 < coin2)
+        {
+          return ("Player 2 wins");
+        }
+        else 
+        {
+          return ("tie game");
+        }
+      }
+    }
+    }
+    if 
   }
 
-  public static void uprightNumberTriangle(int N)
-  {
-    // code solution here
-  }
-
-  public static void starTree()
-  {
-    // code solution here
-  }
-
-  public static void multTable()
-  {
-    // code solution here
-  }
 }
+//   public static void printNTimes(String word, int N)
+//   {
+//     // code solution here
+//   }
+
+//   public static void printNums()
+//   {
+//     // code solution here
+//   }
+
+//   public static void uprightNumberTriangle(int N)
+//   {
+//     // code solution here
+//   }
+
+//   public static void starTree()
+//   {
+//     // code solution here
+//   }
+
+//   public static void multTable()
+//   {
+//     // code solution here
+//   }
+// }
